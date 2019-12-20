ce0fa475d4d873345b5ea2eb4630de33a4bc8f57
todoroki <ryo.konishi@mixi.co.jp>
2019-12-20 15:45:15 +0900

    hold csv in memory

--- lib/table.ex
+++ lib/table.ex
@@ -5,16 +5,15 @@ defmodule EctoCsv.Table do
 
   use GenServer
 
-  defstruct [:format, :parser, :path, :parse_options]
+  defstruct [:format, :parser, :path, :parse_options, :data]
 
   @type t :: %__MODULE__{
           format: :csv | :tsv,
           parser: EctoCsv.Parser.t(),
           path: String.t(),
-          parse_options: Keyword.t()
+          parse_options: Keyword.t(),
+          data: String.t()
         }
-
-  @readonly_mode [:read]
 
   @doc """
   Parameter `name` should be a name to identify the table.
@@ -29,18 +28,19 @@ defmodule EctoCsv.Table do
         format: format,
         parser: parser,
         path: path,
-        parse_options: [skip_headers: false]
+        parse_options: [skip_headers: false],
+        data: File.read!(path)
       }
 
       GenServer.start_link(__MODULE__, state, name: name)
     end
   end
 
-  @spec read(GenServer.name(), Keyword.t()) :: Enum.t()
-  def read(pid, opts \\ []), do: GenServer.call(pid, {:read, opts})
+  @spec read(GenServer.name()) :: Enum.t()
+  def read(pid), do: GenServer.call(pid, :read)
 
-  @spec stream(GenServer.name(), Keyword.t()) :: Enumerable.t()
-  def stream(pid, opts \\ []), do: GenServer.call(pid, {:stream, opts})
+  @spec stream(GenServer.name()) :: Enumerable.t()
+  def stream(pid), do: GenServer.call(pid, :stream)
 
   #
   # GenServer implementations
@@ -51,9 +51,10 @@ defmodule EctoCsv.Table do
   def init(%__MODULE__{} = state), do: {:ok, state}
 
   @impl GenServer
-  def handle_call({:stream, opts}, _from, %__MODULE__{} = state) do
+  def handle_call(:stream, _from, %__MODULE__{data: data} = state) do
     stream =
-      File.stream!(state.path, opts ++ @readonly_mode, :line)
+      data
+      |> String.splitter("\n", trim: true)
       |> state.parser.parse_stream(state.parse_options)
       |> Stream.with_index(1)
       |> Stream.map(fn {data, index} -> {index, [data]} end)
@@ -62,9 +63,10 @@ defmodule EctoCsv.Table do
   end
 
   @impl GenServer
-  def handle_call({:read, opts}, _from, %__MODULE__{} = state) do
+  def handle_call(:read, _from, %__MODULE__{data: data} = state) do
     list =
-      File.stream!(state.path, opts ++ @readonly_mode, :line)
+      data
+      |> String.splitter("\n", trim: true)
       |> state.parser.parse_stream(state.parse_options)
 
     {:reply, list, state}
